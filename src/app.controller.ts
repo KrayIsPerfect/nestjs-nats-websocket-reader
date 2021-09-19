fe07ad13a857045df868afdfacf38ab5952aa63f
Maxim <evilrai27@gmail.com>
2021-09-19 10:00:24 +0000

    Additional task 2: send by TCP and another data type (js object);

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,29 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Transport } from '@nestjs/microservices';
+import { DataTypeEnum } from './utils/data.type.enum';
 
 @Controller('api')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('nats')
-  async sendDataNats(): Promise<void> {
-    await this.appService.sendData(Transport.NATS);
+  @Get('nats/file')
+  async sendFileNats(): Promise<void> {
+    await this.appService.sendData(DataTypeEnum.FILE, Transport.NATS);
   }
 
-  @Get('tcp')
-  async sendData(): Promise<void> {
-    await this.appService.sendData(Transport.TCP);
+  @Get('tcp/file')
+  async sendFileTcp(): Promise<void> {
+    await this.appService.sendData(DataTypeEnum.FILE, Transport.TCP);
+  }
+
+  @Get('nats/object')
+  async sendObjectNats(): Promise<void> {
+    await this.appService.sendData(DataTypeEnum.DATA, Transport.NATS);
+  }
+
+  @Get('tcp/object')
+  async sendObject(): Promise<void> {
+    await this.appService.sendData(DataTypeEnum.DATA, Transport.TCP);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable } from '@nestjs/common';
 import { ClientProxy, Transport } from '@nestjs/microservices';
 import * as fs from 'fs';
 import { timeout } from 'rxjs/operators';
+import { DataTypeEnum } from './utils/data.type.enum';
+import { IData } from './utils/data.interface';
 
 const FILE_NAME = 'test_image.jpg';
 
@@ -14,56 +16,26 @@ export class AppService {
     private readonly clientTcp: ClientProxy,
   ) {}
 
-  async sendData(transport: Transport): Promise<void> {
+  async sendData(type: DataTypeEnum, transport: Transport): Promise<void> {
     try {
       console.log('Sending data...');
-      let data = fs.readFileSync(FILE_NAME);
-      let part = 1;
-      try {
-        if (data.length > 150000) {
-          while (data.length > 150000) {
-            const dataChunk = data.slice(0, 150000);
-            data = data.slice(150000, data.length);
-            const res = await this.sendByTransport(transport, {
-              name: FILE_NAME,
-              type: 'file',
-              part,
-              dataChunk: dataChunk,
-            });
-            console.log(res);
-            if (res.err) {
-              throw new Error(`Error: ${res.err}`);
-            }
-            part++;
-          }
-        }
-        const res = await this.sendByTransport(transport, {
-          name: FILE_NAME,
-          type: 'file',
-          part,
-          dataChunk: data,
-        });
-        console.log(res);
-        if (res.err) {
-          throw new Error(`Error: ${res.err}`);
-        }
-      } catch (err) {
-        console.log(err.message);
-        const res = await this.sendByTransport(transport, {
-          name: FILE_NAME,
-          type: 'file',
-          part,
-          dataChunk: null,
-        });
-
-        console.log(res);
+      if (type === DataTypeEnum.FILE) {
+        await this.sendFile(transport);
+      } else if (type === DataTypeEnum.DATA) {
+        await this.sendObject(transport);
+      } else {
+        throw new Error('Unknown datatype');
       }
+      console.log('Data send');
     } catch (e) {
       console.log(e);
     }
   }
 
-  private async sendByTransport(transport: Transport, data) {
+  private async sendByTransport(
+    transport: Transport,
+    data: IData,
+  ): Promise<any> {
     let res = null;
     if (transport === Transport.TCP) {
       res = await this.clientTcp
@@ -76,8 +48,67 @@ export class AppService {
         .pipe(timeout(10000))
         .toPromise();
     } else {
-      console.log('Unknown transport');
+      throw new Error('Unknown transport');
     }
     return res;
+  }
+
+  private async sendFile(transport: Transport): Promise<void> {
+    let data = fs.readFileSync(FILE_NAME);
+    let part = 1;
+    try {
+      if (data.length > 150000) {
+        while (data.length > 150000) {
+          const dataChunk = data.slice(0, 150000);
+          data = data.slice(150000, data.length);
+          const res = await this.sendByTransport(transport, {
+            name: FILE_NAME,
+            type: DataTypeEnum.FILE,
+            part,
+            data,
+          });
+          console.log(res);
+          if (res.err) {
+            throw new Error(`Error: ${res.err}`);
+          }
+          part++;
+        }
+      }
+      const res = await this.sendByTransport(transport, {
+        name: FILE_NAME,
+        type: DataTypeEnum.FILE,
+        part,
+        data,
+      });
+      console.log(res);
+      if (res.err) {
+        throw new Error(`Error: ${res.err}`);
+      }
+    } catch (err) {
+      console.log(err.message);
+      const res = await this.sendByTransport(transport, {
+        name: FILE_NAME,
+        type: DataTypeEnum.FILE,
+        part,
+        data: null,
+      });
+
+      console.log(res);
+    }
+  }
+
+  private async sendObject(transport: Transport): Promise<void> {
+    try {
+      const res = await this.sendByTransport(transport, {
+        type: DataTypeEnum.DATA,
+        data: { id: 123, description: 'Test another data format' },
+      });
+      console.log(res);
+      if (res.err) {
+        throw new Error(`Error: ${res.err}`);
+      }
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
